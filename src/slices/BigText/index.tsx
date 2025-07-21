3e9d2781a3874dc2ea9c0c1e89cc24140983c8dc
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-07-21 16:09:23 +0530

    .

--- src/slices/BigText/index.tsx
+++ src/slices/BigText/index.tsx
@@ -15,16 +15,16 @@ const BigText = ({ slice }: BigTextProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="min-h-screen w-screen overflow-hidden bg-[#FE6334] text-[#FEE832]"
+      className="w-screen overflow-hidden bg-[#FE6334] text-[#FEE832]"
     >
       <h2 className="grid w-full gap-[3vw] py-10 text-center font-black uppercase leading-[.7]">
-        <div className="text-[34vw]">Soda</div>
-        <div className="grid gap-[3vw] text-[34vw] md:flex md:text-[11vw]">
-          <span className="inline-block">that </span>
-          <span className="inline-block max-md:text-[27vw]">makes </span>
-          <span className="inline-block max-md:text-[40vw]">you </span>
+        <div className="text-[30vw]">Fizz</div>
+        <div className="grid gap-[3vw] text-[30vw] md:flex md:text-[11vw] md:items-center md:justify-center">
+          <span className="inline-block">full </span>
+          <span className="inline-block max-md:text-[27vw]">of </span>
+          <span className="inline-block max-md:text-[28vw]">happy </span>
         </div>
-        <div className="text-[32vw]">Smile</div>
+        <div className="text-[25vw]">vibes</div>
       </h2>
     </section>
   );
